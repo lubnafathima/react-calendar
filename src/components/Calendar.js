0db61186ed0cac7912ab76722c0bcbcfc77a33f5
lubna-sensegrass <lubnafathima275@gmail.com>
2024-06-01 17:18:09 +0530

    feat: added full calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,22 @@
-import Header from "./Header";
-import Main from "./Main";
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const Calendar = () => {
   return (
     <div className="calendar">
-      <Header />
-      <Main />
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today",
+          center: "title",
+          end: "prev,next"
+        }}
+        height={"95vh"}
+      />
     </div>
   );
 };
